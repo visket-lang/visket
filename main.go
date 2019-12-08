990437fce51f1ceacec208fea9195236ddf0edb0
arata <arata04962@gmail.com>
2019-12-08 22:47:34 +0900

    [update] Compile program to exit with given number

--- main.go
+++ main.go
@@ -2,8 +2,25 @@ package main
 
 import (
   "fmt"
+  "log"
+  "os"
+  "strconv"
 )
 
 func main() {
-  fmt.Println("Hello world!")
+  if len(os.Args) != 2 {
+    fmt.Println("Usage: solitude <number>")
+    os.Exit(1)
+  }
+
+  n, err := strconv.Atoi(os.Args[1])
+  if err != nil {
+    log.Fatal(err)
+    os.Exit(1)
+  }
+
+  fmt.Println("define i32 @main() nounwind {")
+  fmt.Printf("  ret i32 %d\n", n)
+  fmt.Println("}")
+
 }
