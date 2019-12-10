19312db99daa378fefe51e3b8882f9a22ff5591d
arata <arata04962@gmail.com>
2019-12-10 19:47:30 +0900

    [update] Split codegen functions

--- codegen/CodeGen.go
+++ codegen/CodeGen.go
@@ -6,6 +6,9 @@ import (
 	"github.com/arata-nvm/Solitude/parser"
 )
 
+type Pointer int
+type Value int
+
 type CodeGen struct {
 	p     *parser.Parser
 	index int
@@ -21,58 +24,92 @@ func New(p *parser.Parser) *CodeGen {
 
 func (c *CodeGen) GenerateCode() {
 	program := c.p.ParseProgram()
-	fmt.Println("define i32 @main() nounwind {")
+	c.gen("define i32 @main() nounwind {\n")
+	c.genExpr(program.Code)
 
-	c.gen(program.Code)
-
+	c.comment("  ; Ret\n")
 	numProcLastIndex := c.index
-	c.index++
-	regIndex := c.index
-
-	fmt.Printf("  %%%d = load i32, i32* %%%d, align 4\n", regIndex, numProcLastIndex)
-	fmt.Printf("  ret i32 %%%d\n", regIndex)
-	fmt.Println("}")
+	regIndex := c.genLoad(numProcLastIndex)
+	c.gen("  ret i32 %%%d\n", regIndex)
+	c.gen("}\n")
 }
 
-func (c *CodeGen) gen(node ast.Node) {
+func (c *CodeGen) genExpr(node ast.Node) {
 	switch node := node.(type) {
 	case *ast.InfixExpression:
 		c.genInfix(node)
 	case *ast.IntegerLiteral:
-		c.index++
-		fmt.Println("  ; Assign")
-		fmt.Printf("  %%%d = alloca i32, align 4\n", c.index)
-		fmt.Printf("  store i32 %d, i32* %%%d\n", node.Value, c.index)
+		c.comment("  ; Assign\n")
+		c.genAlloca()
+		c.genStoreImmediate(node.Value, c.index)
 	}
 }
 
 func (c *CodeGen) genInfix(ie *ast.InfixExpression) {
-	c.gen(ie.Left)
+	c.comment("  ; Infix\n")
+
+	c.genExpr(ie.Left)
 	lhsIndex := c.index
-	c.gen(ie.Right)
+	c.genExpr(ie.Right)
 	rhsIndex := c.index
 
-	c.index++
-	lhsRegIndex := c.index
-	fmt.Printf("  %%%d= load i32, i32* %%%d, align 4\n", c.index, lhsIndex)
+	c.comment("  ; Op\n")
 
-	c.index++
-	rhsRegIndex := c.index
-	fmt.Printf("  %%%d = load i32, i32* %%%d, align 4\n", c.index, rhsIndex)
+	lhsRegIndex := c.genLoad(lhsIndex)
+	rhsRegIndex := c.genLoad(rhsIndex)
 
-	c.index++
-	resRegIndex := c.index
+	var resRegIndex int
 
 	switch ie.Operator {
 	case "+":
-		fmt.Printf("  %%%d = add i32 %%%d, %%%d\n", c.index, lhsRegIndex, rhsRegIndex)
+		c.comment("  ; Add\n")
+		resRegIndex = c.genAdd(lhsRegIndex, rhsRegIndex)
 	case "-":
-		fmt.Printf("  %%%d = sub i32 %%%d, %%%d\n", c.index, lhsRegIndex, rhsRegIndex)
+		c.comment("  ; Sub\n")
+		resRegIndex = c.genSub(lhsRegIndex, rhsRegIndex)
 	}
 
-	c.index++
-	resMemIndex := c.index
-	fmt.Printf("  %%%d = alloca i32, align 4\n", c.index)
+	resMemIndex := c.genAlloca()
+	c.genStore(resRegIndex, resMemIndex)
+}
 
-	fmt.Printf("  store i32 %%%d, i32* %%%d, align 4\n", resRegIndex, resMemIndex)
+func (c *CodeGen) gen(format string, a ...interface{}) {
+	fmt.Printf(format, a...)
+}
+
+func (c *CodeGen) comment(format string, a ...interface{}) {
+	fmt.Println("")
+	fmt.Printf(format, a...)
+}
+
+func (c *CodeGen) genAlloca() int {
+	c.index++
+	c.gen("  %%%d = alloca i32, align 4\n", c.index)
+	return c.index
+}
+
+func (c *CodeGen) genStore(pointer1 int, pointer2 int) {
+	c.gen("  store i32 %%%d, i32* %%%d\n", pointer1, pointer2)
+}
+
+func (c *CodeGen) genStoreImmediate(value int, pointer int) {
+	c.gen("  store i32 %d, i32* %%%d\n", value, pointer)
+}
+
+func (c *CodeGen) genLoad(pointer int) int {
+	c.index++
+	c.gen("  %%%d = load i32, i32* %%%d, align 4\n", c.index, pointer)
+	return c.index
+}
+
+func (c *CodeGen) genAdd(op1 int, op2 int) int {
+	c.index++
+	c.gen("  %%%d = add i32 %%%d, %%%d\n", c.index, op1, op2)
+	return c.index
+}
+
+func (c *CodeGen) genSub(op1 int, op2 int) int {
+	c.index++
+	c.gen("  %%%d = sub i32 %%%d, %%%d\n", c.index, op1, op2)
+	return c.index
 }
