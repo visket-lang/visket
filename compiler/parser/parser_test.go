66be12446aff7f36523e1ecec71c6b72e939050a
arata <arata04962@gmail.com>
2020-02-28 15:47:07 +0900

    [update] Parser test uses ast.Show

--- compiler/parser/parser_test.go
+++ compiler/parser/parser_test.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"github.com/arata-nvm/Solitude/compiler/ast"
 	"github.com/arata-nvm/Solitude/compiler/lexer"
 	"testing"
 )
@@ -10,13 +11,13 @@ func TestParseProgram(t *testing.T) {
 		input    string
 		expected string
 	}{
-		{"func f(a: int) {return 1}", "func Ident(f)(Ident(a): Type(int)): Type(void) {return Int(1)}"},
-		{"func hoge(fuga: int): int {return fuga}", "func Ident(hoge)(Ident(fuga): Type(int)): Type(int) {return Ident(fuga)}"},
+		{"func f(a: int) {return 1}", "(def-func f(a: int): void ((return 1)))"},
+		{"func hoge(fuga: int): int {return fuga}", "(def-func hoge(fuga: int): int ((return fuga)))"},
 
-		{"func num(): int {return 2} func main() {return num()}", "func Ident(num)(): Type(int) {return Int(2)}func Ident(main)(): Type(void) {return Call(Ident(num)())}"},
-		{"func add(n: int): int {return n + 2} func main() {return num(1)}", "func Ident(add)(Ident(n): Type(int)): Type(int) {return Infix(Ident(n) + Int(2))}func Ident(main)(): Type(void) {return Call(Ident(num)(Int(1)))}"},
-		{"func add(a: int, b: int): int {return a + b} func main() {return num(1, 2)}", "func Ident(add)(Ident(a): Type(int), Ident(b): Type(int)): Type(int) {return Infix(Ident(a) + Ident(b))}func Ident(main)(): Type(void) {return Call(Ident(num)(Int(1),Int(2)))}"},
-		{"struct Foo { X int Y float }", "struct Ident(Foo) { Ident(X) int Ident(Y) float }"},
+		{"func num(): int {return 2} func main() {return num()}", "(def-func num(): int ((return 2)))(def-func main(): void ((return (func-call num()))))"},
+		{"func add(n: int): int {return n + 2} func main() {return num(1)}", "(def-func add(n: int): int ((return (n + 2))))(def-func main(): void ((return (func-call num(1)))))"},
+		{"func add(a: int, b: int): int {return a + b} func main() {return num(1, 2)}", "(def-func add(a: int, b: int): int ((return (a + b))))(def-func main(): void ((return (func-call num(1, 2)))))"},
+		{"struct Foo { X int Y float }", "(struct Foo(X: intY: float))"},
 	}
 
 	for i, test := range tests {
@@ -24,7 +25,7 @@ func TestParseProgram(t *testing.T) {
 		p := New(l)
 		program := p.ParseProgram()
 		checkParserErrors(t, p)
-		actual := program.Inspect()
+		actual := ast.Show(program)
 
 		if actual != test.expected {
 			t.Fatalf("tests[%d] - expected=%q, got=%q", i, test.expected, actual)
@@ -37,60 +38,60 @@ func TestParseStatement(t *testing.T) {
 		input    string
 		expected string
 	}{
-		{"0", "Int(0)"},
-		{"42", "Int(42)"},
-		{"32.0", "Float(32.000000)"},
+		{"0", "0"},
+		{"42", "42"},
+		{"32.0", "32.0"},
 
-		{"4 + 4", "Infix(Int(4) + Int(4))"},
-		{"4 - 4", "Infix(Int(4) - Int(4))"},
-		{"4 * 4", "Infix(Int(4) * Int(4))"},
-		{"4 / 4", "Infix(Int(4) / Int(4))"},
-		{"4 % 4", "Infix(Int(4) % Int(4))"},
-		{"4 << 4", "Infix(Int(4) << Int(4))"},
-		{"4 >> 4", "Infix(Int(4) >> Int(4))"},
-		{"4 == 4", "Infix(Int(4) == Int(4))"},
-		{"4 != 4", "Infix(Int(4) != Int(4))"},
-		{"4 < 4", "Infix(Int(4) < Int(4))"},
-		{"4 <= 4", "Infix(Int(4) <= Int(4))"},
-		{"4 > 4", "Infix(Int(4) > Int(4))"},
-		{"4 >= 4", "Infix(Int(4) >= Int(4))"},
+		{"4 + 4", "(4 + 4)"},
+		{"4 - 4", "(4 - 4)"},
+		{"4 * 4", "(4 * 4)"},
+		{"4 % 4", "(4 % 4)"},
+		{"4 / 4", "(4 / 4)"},
+		{"4 << 4", "(4 << 4)"},
+		{"4 >> 4", "(4 >> 4)"},
+		{"4 == 4", "(4 == 4)"},
+		{"4 != 4", "(4 != 4)"},
+		{"4 < 4", "(4 < 4)"},
+		{"4 <= 4", "(4 <= 4)"},
+		{"4 > 4", "(4 > 4)"},
+		{"4 >= 4", "(4 >= 4)"},
 
-		{"4 + 4 * 4", "Infix(Int(4) + Infix(Int(4) * Int(4)))"},
-		{"4 * 4 + 4", "Infix(Infix(Int(4) * Int(4)) + Int(4))"},
+		{"4 + 4 * 4", "(4 + (4 * 4))"},
+		{"4 * 4 + 4", "((4 * 4) + 4)"},
 
-		{"a += 1", "Ident(a) = Infix(Ident(a) + Int(1))"},
-		{"b -= 2", "Ident(b) = Infix(Ident(b) - Int(2))"},
-		{"c *= 3", "Ident(c) = Infix(Ident(c) * Int(3))"},
-		{"d /= 4", "Ident(d) = Infix(Ident(d) / Int(4))"},
-		{"e %= 5", "Ident(e) = Infix(Ident(e) % Int(5))"},
-		{"f <<= 6", "Ident(f) = Infix(Ident(f) << Int(6))"},
-		{"g >>= 7", "Ident(g) = Infix(Ident(g) >> Int(7))"},
+		{"a += 1", "(a = (a + 1))"},
+		{"b -= 2", "(b = (b - 2))"},
+		{"c *= 3", "(c = (c * 3))"},
+		{"d /= 4", "(d = (d / 4))"},
+		{"e %= 5", "(e = (e % 5))"},
+		{"f <<= 6", "(f = (f << 6))"},
+		{"g >>= 7", "(g = (g >> 7))"},
 
-		{"a += 1 + 2", "Ident(a) = Infix(Ident(a) + Infix(Int(1) + Int(2)))"},
+		{"a += 1 + 2", "(a = (a + (1 + 2)))"},
 
-		{"var hoge = 1", "var Ident(hoge) = Int(1)"},
-		{"var fuga = hoge * 2 + 2", "var Ident(fuga) = Infix(Infix(Ident(hoge) * Int(2)) + Int(2))"},
+		{"var hoge = 1", "(var hoge = 1)"},
+		{"var fuga = hoge * 2 + 2", "(var fuga = ((hoge * 2) + 2))"},
 
-		{"return 0", "return Int(0)"},
-		{"return hoge", "return Ident(hoge)"},
+		{"return 0", "(return 0)"},
+		{"return hoge", "(return hoge)"},
 
-		{"func f(a: int) {return 1}", "func Ident(f)(Ident(a): Type(int)): Type(void) {return Int(1)}"},
-		{"func hoge(fuga: int) {return fuga}", "func Ident(hoge)(Ident(fuga): Type(int)): Type(void) {return Ident(fuga)}"},
+		{"func f(a: int) {return 1}", "(def-func f(a: int): void ((return 1)))"},
+		{"func hoge(fuga: int) {return fuga}", "(def-func hoge(fuga: int): void ((return fuga)))"},
 
-		{"if 1 { 1 } else { 0 }", "if Int(1) {Int(1)} else {Int(0)}"},
+		{"if 1 { 1 } else { 0 }", "(if 1(1)(0))"},
 
-		{"while 1 { 1 }", "while Int(1) {Int(1)}"},
+		{"while 1 { 1 }", "(while 1(1))"},
 
-		{"a = a + 1", "Ident(a) = Infix(Ident(a) + Int(1))"},
+		{"a = a + 1", "(a = (a + 1))"},
 
-		{"for i in 0..10 {1}", "for var Ident(i) = Int(0); Infix(Ident(i) <= Int(10)); Ident(i) = Infix(Ident(i) + Int(1)) {Int(1)}"},
-		{"for var i = 0; i < 10; i = i + 1 {1}", "for var Ident(i) = Int(0); Infix(Ident(i) < Int(10)); Ident(i) = Infix(Ident(i) + Int(1)) {Int(1)}"},
+		{"for i in 0..10 {1}", "(for (var i = 0); (i <= 10); (i = (i + 1))(1))"},
+		{"for var i = 0; i < 10; i = i + 1 {1}", "(for (var i = 0); (i < 10); (i = (i + 1))(1))"},
 
-		{"array[1]", "Ident(array)[Int(1)]"},
-		{"array[a * 10 + 1]", "Ident(array)[Infix(Infix(Ident(a) * Int(10)) + Int(1))]"},
+		{"array[1]", "(array[1])"},
+		{"array[a * 10 + 1]", "(array[((a * 10) + 1)])"},
 
-		{"new Foo", "new Ident(Foo)"},
-		{"foo.X", "Ident(foo).Ident(X)"},
+		{"new Foo", "(new Foo)"},
+		{"foo.X", "(foo.X)"},
 	}
 
 	for i, test := range tests {
@@ -98,7 +99,7 @@ func TestParseStatement(t *testing.T) {
 		p := New(l)
 		program := p.parseStatement()
 		checkParserErrors(t, p)
-		actual := program.Inspect()
+		actual := ast.Show(program)
 
 		if actual != test.expected {
 			t.Fatalf("tests[%d] - expected=%q, got=%q", i, test.expected, actual)
