51fd41bc1a8759e801006688acce4040e481a118
arata <arata04962@gmail.com>
2020-03-11 02:22:08 +0900

    [fix] Test cases

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ try() {
   expected="$1"
   input="$2"
 
-  echo 'import "lib/testlib"' > tmp.sl
+  echo 'import "lib/std"' > tmp.sl
   echo "$input" >> tmp.sl
   cat tmp.sl
   $TARGET $OPT -o tmp tmp.sl > /dev/null
@@ -24,174 +24,174 @@ try() {
   fi
 }
 
-try 0 "fun main() { print(0) }"
-try 42 "fun main() { print(42) }"
+try 0 "fun main() { printi(0) }"
+try 42 "fun main() { printi(42) }"
 
-try 4 "fun main() { print(2+2) }"
-try 52 "fun main() { print(83-31) }"
+try 4 "fun main() { printi(2+2) }"
+try 52 "fun main() { printi(83-31) }"
 
-try 10 "fun main() { print(10   -   0) }"
-try 10 "fun main() { print(10 + 10 + 10 - 20) }"
-try 0 "fun main() { print(10 - 10 + 10 - 10) }"
-try 20 "fun main() { print(20 - 0 + 0 - 0) }"
+try 10 "fun main() { printi(10   -   0) }"
+try 10 "fun main() { printi(10 + 10 + 10 - 20) }"
+try 0 "fun main() { printi(10 - 10 + 10 - 10) }"
+try 20 "fun main() { printi(20 - 0 + 0 - 0) }"
 
-try 30 "fun main() { print(10 + 10 * 2) }"
-try 102 "fun main() { print(10 * 10 + 2) }"
-try 200 "fun main() { print(10 * 10 * 2) }"
+try 30 "fun main() { printi(10 + 10 * 2) }"
+try 102 "fun main() { printi(10 * 10 + 2) }"
+try 200 "fun main() { printi(10 * 10 * 2) }"
 
-try 40 "fun main() { print((10 + 10) * 2) }"
-try 120 "fun main() { print(10 * (10 + 2)) }"
+try 40 "fun main() { printi((10 + 10) * 2) }"
+try 120 "fun main() { printi(10 * (10 + 2)) }"
 
-try 10 "fun main() { print((10 + 10) / 2) }"
-try 5 "fun main() { print(60 / (10 + 2)) }"
+try 10 "fun main() { printi((10 + 10) / 2) }"
+try 5 "fun main() { printi(60 / (10 + 2)) }"
 
-try 1 "fun main() { print(9 % 2) }"
-try 3 "fun main() { print(1 + 5 % 3) }"
+try 1 "fun main() { printi(9 % 2) }"
+try 3 "fun main() { printi(1 + 5 % 3) }"
 
-try 16 "fun main() { print(2 << 3) }"
-try 2 "fun main() { print(16 >> 3) }"
+try 16 "fun main() { printi(2 << 3) }"
+try 2 "fun main() { printi(16 >> 3) }"
 
-try 10 "fun main() { print(120 + -110) }"
-try 0 "fun main() { print(-(-10 - (-10))) }"
+try 10 "fun main() { printi(120 + -110) }"
+try 0 "fun main() { printi(-(-10 - (-10))) }"
 
-try 1 "fun main() { if 10 == 10 { print(1) } else { print(0) }}"
-try 0 "fun main() { if 10 == 9 { print(1) } else { print(0) }}"
-try 1 "fun main() { if 10 != 9 { print(1) } else { print(0) }}"
-try 0 "fun main() { if 10 != 10 { print(1) } else { print(0) }}"
+try 1 "fun main() { if 10 == 10 { printi(1) } else { printi(0) }}"
+try 0 "fun main() { if 10 == 9 { printi(1) } else { printi(0) }}"
+try 1 "fun main() { if 10 != 9 { printi(1) } else { printi(0) }}"
+try 0 "fun main() { if 10 != 10 { printi(1) } else { printi(0) }}"
 
-try 1 "fun main() { if 9 < 10 { print(1) } else { print(0) }}"
-try 0 "fun main() { if 10 < 10 { print(1) } else { print(0) }}"
-try 1 "fun main() { if 10 <= 10 { print(1) } else { print(0) }}"
-try 0 "fun main() { if 10 <= 9 { print(1) } else { print(0) }}"
-try 1 "fun main() { if 10 > 9 { print(1) } else { print(0) }}"
-try 0 "fun main() { if 10 > 10 { print(1) } else { print(0) }}"
-try 1 "fun main() { if 10 >= 10 { print(1) } else { print(0) }}"
-try 0 "fun main() { if 9 >= 10 { print(1) } else { print(0) }}"
+try 1 "fun main() { if 9 < 10 { printi(1) } else { printi(0) }}"
+try 0 "fun main() { if 10 < 10 { printi(1) } else { printi(0) }}"
+try 1 "fun main() { if 10 <= 10 { printi(1) } else { printi(0) }}"
+try 0 "fun main() { if 10 <= 9 { printi(1) } else { printi(0) }}"
+try 1 "fun main() { if 10 > 9 { printi(1) } else { printi(0) }}"
+try 0 "fun main() { if 10 > 10 { printi(1) } else { printi(0) }}"
+try 1 "fun main() { if 10 >= 10 { printi(1) } else { printi(0) }}"
+try 0 "fun main() { if 9 >= 10 { printi(1) } else { printi(0) }}"
 
-try 1 "fun main() { if 9.0 < 10.0 { print(1) } else { print(0) }}"
-try 0 "fun main() { if 10.0 < 10.0 { print(1) } else { print(0) }}"
-try 1 "fun main() { if 10.0 <= 10.0 { print(1) } else { print(0) }}"
-try 0 "fun main() { if 10.0 <= 9.0 { print(1) } else { print(0) }}"
-try 1 "fun main() { if 10.0 > 9.0 { print(1) } else { print(0) }}"
-try 0 "fun main() { if 10.0 > 10.0 { print(1) } else { print(0) }}"
-try 1 "fun main() { if 10.0 >= 10.0 { print(1) } else { print(0) }}"
-try 0 "fun main() { if 9.0 >= 10.0 { print(1) } else { print(0) }}"
+try 1 "fun main() { if 9.0 < 10.0 { printi(1) } else { printi(0) }}"
+try 0 "fun main() { if 10.0 < 10.0 { printi(1) } else { printi(0) }}"
+try 1 "fun main() { if 10.0 <= 10.0 { printi(1) } else { printi(0) }}"
+try 0 "fun main() { if 10.0 <= 9.0 { printi(1) } else { printi(0) }}"
+try 1 "fun main() { if 10.0 > 9.0 { printi(1) } else { printi(0) }}"
+try 0 "fun main() { if 10.0 > 10.0 { printi(1) } else { printi(0) }}"
+try 1 "fun main() { if 10.0 >= 10.0 { printi(1) } else { printi(0) }}"
+try 0 "fun main() { if 9.0 >= 10.0 { printi(1) } else { printi(0) }}"
 
 try 10 \
 "fun main() {
   var a = 10
-  print(a)
+  printi(a)
 }"
 
 try 5 \
 "fun main() {
   var a = 10
-  print(a - 5)
+  printi(a - 5)
 }"
 
 try 10 \
 "fun main() {
   var a = 5
   a = a + 5
-  print(a)
+  printi(a)
 }"
 
 try 7 \
 "fun main() {
   var a = 5
   a += 2
-  print(a)
+  printi(a)
 }"
 
 try 3 \
 "fun main() {
   var a = 5
   a -= 2
-  print(a)
+  printi(a)
 }"
 
 try 10 \
 "fun main() {
   var a = 5
   a *= 2
-  print(a)
+  printi(a)
 }"
 
 try 2 \
 "fun main() {
   var a = 5
   a /= 2
-  print(a)
+  printi(a)
 }"
 
 try 1 \
 "fun main() {
   var a = 5
   a %= 2
-  print(a)
+  printi(a)
 }"
 
 try 12 \
 "fun main() {
   var a = 3
   a <<= 2
-  print(a)
+  printi(a)
 }"
 
 try 5 \
 "fun main() {
   var a = 20
   a >>= 2
-  print(a)
+  printi(a)
 }"
 
 try 2 "fun num(): int { return 2 }
-fun main() { print(num()) }"
+fun main() { printi(num()) }"
 try 4 "fun add(n: int): int { return n + 2 }
-fun main() { print(add(2)) }"
+fun main() { printi(add(2)) }"
 
 try 6 "fun add(a: int, b: int): int { return a + b }
-fun main() { print(add(2, 4)) }"
+fun main() { printi(add(2, 4)) }"
 
 try 2 \
 "fun main() {
   if true {
-    print(2)
+    printi(2)
     return;
   } else {
-    print(1)
+    printi(1)
     return;
   }
-  print(0)
+  printi(0)
 }"
 
 try 1 \
 "fun main() {
   if false {
-    print(2)
+    printi(2)
     return;
   } else {
-    print(1)
+    printi(1)
     return;
   }
-  print(0)
+  printi(0)
 }"
 
 try 2 \
 "fun main() {
   if true {
     if false {
-      print(3)
+      printi(3)
       return;
     } else {
-      print(2)
+      printi(2)
       return;
     }
   } else {
-    print(1)
+    printi(1)
     return;
   }
-  print(0)
+  printi(0)
 }"
 
 try 0 \
@@ -199,19 +199,19 @@ try 0 \
   if true {
 
   }
-  print(0)
+  printi(0)
 }"
 
 try 1 \
 "fun main() {
   if true {
-    print(1)
+    printi(1)
     return;
   } else {
 
   }
 
-  print(2)
+  printi(2)
 }"
 
 try 0 \
@@ -219,23 +219,23 @@ try 0 \
   if true {
 
   } else {
-    print(1)
+    printi(1)
     return;
   }
 
-  print(0)
+  printi(0)
 }"
 
 try 0 \
 "fun main() {
   var n: int
   if n == 1 {
-    print(n)
+    printi(n)
     return;
   } else {
 
   }
-  print(n)
+  printi(n)
 }"
 
 try 55 \
@@ -247,20 +247,20 @@ try 55 \
 }
 
 fun main() {
-  print(fib(10))
+  printi(fib(10))
 }"
 
 try 10 \
 "fun main() {
   var a = 5
   if a > 3 {
-    print(10)
+    printi(10)
     return;
   } if a > 2 {
-    print(5)
+    printi(5)
     return;
   }
-  print(0)
+  printi(0)
 }"
 
 try 45 \
@@ -269,7 +269,7 @@ try 45 \
   for var i = 0; i < 10; i = i + 1 {
     sum = sum + i
   }
-  print(sum)
+  printi(sum)
 }"
 
 
@@ -279,13 +279,13 @@ try 45 \
   for var i = 0; i <= 9; i = i + 1 {
     sum = sum + i
   }
-  print(sum)
+  printi(sum)
 }"
 
 try 10 \
 "fun main() {
-// print(0)
-  print(10) // 0
+// printi(0)
+  printi(10) // 0
 }"
 
 try 10 \
@@ -296,7 +296,7 @@ try 10 \
     i = i + 1 // {
   {
   }
-  print(i)
+  printi(i)
 }"
 
 try 55 \
@@ -305,13 +305,13 @@ try 55 \
   for i in 0..10 {
     sum += i
   }
-  print(sum)
+  printi(sum)
 }"
 
 try 0 \
 "fun main() {
   var a: [3]int
-  print(a[0])
+  printi(a[0])
 }"
 
 try 3 \
@@ -321,12 +321,12 @@ try 3 \
   a[0] = 1
   a[b - 1] = 2
   a[b * 1] = b + 1
-  print(a[b])
+  printi(a[b])
 }"
 
 try 20 \
 "fun main() {
-  print(test1(10))
+  printi(test1(10))
 }
 
 fun test1(a: int): int {
@@ -342,7 +342,7 @@ fun main() {
   foo1.X = 10
   var foo2 = new Foo
   foo2.X = 20
-  print(foo1.X)
+  printi(foo1.X)
 }"
 
 
@@ -357,7 +357,7 @@ fun main() {
   var bar = new Bar
   bar.A = new Foo
   bar.A.X = 20
-  print(bar.A.X)
+  printi(bar.A.X)
 }"
 
 try 0 \
@@ -366,7 +366,7 @@ try 0 \
 }
 fun main() {
   var foo1 = new Foo
-  print(foo1.X)
+  printi(foo1.X)
 }"
 
 try 0 \
@@ -378,7 +378,7 @@ struct Bar {
 }
 fun main() {
   var bar = new Bar
-  print(bar.A.X)
+  printi(bar.A.X)
 }"
 
 try 10 \
@@ -388,7 +388,7 @@ try 10 \
 fun main() {
   var i = 0
   test(i)
-  print(i)
+  printi(i)
 }"
 
 try 5 \
@@ -401,7 +401,7 @@ fun test2(ref i: int) {
 fun main() {
   var i = 0
   test1(i)
-  print(i)
+  printi(i)
 }"
 
 try 10 \
@@ -414,47 +414,47 @@ fun test(ref foo: Foo) {
 fun main() {
   var foo: Foo
   test(foo)
-  print(foo.X)
+  printi(foo.X)
 }"
 
 try 5 \
 "var i = 5
-fun main() { print(i) }"
+fun main() { printi(i) }"
 
 try 0 \
 "var i: int
-fun main() { print(i) }"
+fun main() { printi(i) }"
 
 try 10 \
 "var i: int = 5
 fun main() {
   i = 10
-  print(i)
+  printi(i)
 }"
 
 try 9 \
 "module Lib {
   fun test(): int { return 9 }
 }
-fun main() { print(Lib::test()) }"
+fun main() { printi(Lib::test()) }"
 
 try 4 \
 "module Lib {
   fun test(): int { return 3 }
 }
 fun test(): int { return 1 }
-fun main() { print(Lib::test() + test()) }"
+fun main() { printi(Lib::test() + test()) }"
 
 try 1 \
 "fun main() {
   var s = \"hoge\nfugapiyo\"
   if s[0] == 'h' {
     if s[4] == '\n' {
-      print(1)
+      printi(1)
       return;
     }
   }
-  print(0)
+  printi(0)
 }"
 
 
